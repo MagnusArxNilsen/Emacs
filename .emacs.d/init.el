945525430945af82fe2021d7308d3b44f76911fa
Magnus Arneberg <60578488+MagnusArxNilsen@users.noreply.github.com>
2022-11-10 20:37:57 +0100

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,8 @@
+;;
+;;
+
+(bind-key "C-M-<up>" 'enlarge-window)
+(bind-key "C-M-<right>" 'enlarge-window-horizontally)
+(bind-key "C-M-<down>" 'shrink-window)
+(bind-key "C-M-<left>" 'shrink-window-horizontally)
 
