f423985891f185ce85d0d23eac93e56e7cb9ca5d
Magnus Arneberg <60578488+MagnusArxNilsen@users.noreply.github.com>
2022-11-25 12:52:45 +0100

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -127,6 +127,15 @@
 ;; ----
 ;; -- Setup Script
 
+(if (eq system-type 'windows-nt)
+    (progn (setq ispell-local-dictionary "en_US")
+	   (setq ispell-local-dictionary-alist
+		 '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']"
+		    nil ("-d" "en_US") nil utf-8)))
+	   (setq ispell-program-name "hunspell")
+	   (setq ispell-hunspell-dict-paths-alist
+		 '(("en_US" "~/.emacs.d/hunspell/en_US.aff")))))
+
 (if (eq window-system nil)
     (set-face-attribute 'default nil :background "black"))
 
