b5f1588af5d2d40b2f1d0280ce59cdfa1f31b3cf
Magnus Arneberg <60578488+MagnusArxNilsen@users.noreply.github.com>
2022-11-25 12:32:01 +0100

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -127,13 +127,15 @@
 ;; ----
 ;; -- End Script
 
+(if (eq window-system nil)
+    (set-face-attribute 'default nil :background "black"))
+
 (split-window-vertically)
 (other-window 1)
 (eshell)
 (shrink-window 10)
 (other-window 1)
 (cd "path/to/work/dir")
-
 
 ;; --
 ;; ---- ~/.emacs.d/init.el
